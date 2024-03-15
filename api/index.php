96e92fbbf60d0c136c29637524724f33f0964c24
Oceanofprojects <projectocean101@gmail.com>
2024-03-15 22:50:14 +0530

    old bp & v new API

--- api/index.php
+++ api/index.php
@@ -25,9 +25,6 @@ $password = $_ENV['PG_PASSWORD'];
 $endpoint = $_ENV['PG_ENDPOINT'];
 
 $connection_string = "host=" . $host . " port=" . $port . " dbname=" . $db . " user=" . $user . " password=" . $password . " options='endpoint=" . $endpoint . "' sslmode=require";
-print_r($_ENV);
-echo "<br><br.<br>";
-echo $connection_string;
 $dbconn = pg_connect($connection_string);
 
 if (!$dbconn) {
