ccd046d1ab4c3bbbc66127f201df74b77b8e7e26
Oceanofprojects <projectocean101@gmail.com>
2024-03-15 23:20:01 +0530

    v test api

--- api/index.php
+++ api/index.php
@@ -3,11 +3,28 @@
 
 header('Content-Type: application/json');
 
-if($_SERVER['REQUEST_METHOD'] === 'GET'){
+if($_GET['api'] == 'test'){
 	$response = array(
 		'message'=>'Hello world'
 	);
 	echo json_encode($response);
+}else if($_GET['api'] == 'live'){
+	$host = $_ENV['PG_HOST'];
+	$port = $_ENV['PG_PORT'];
+	$db = $_ENV['PG_DB'];
+	$user = $_ENV['PG_USER'];
+	$password = $_ENV['PG_PASSWORD'];
+	$endpoint = $_ENV['PG_ENDPOINT'];
+
+	$connection_string = "host=" . $host . " port=" . $port . " dbname=" . $db . " user=" . $user . " password=" . $password . " options='endpoint=" . $endpoint . "' sslmode=require";
+	$dbconn = pg_connect($connection_string);
+
+	if (!$dbconn) {
+	    die("Connection failed: " . pg_last_error());
+	}
+
+	echo "Connected successfully";
+
 }else{
 	http_response_code(405);
 	$response = array(
@@ -17,27 +34,5 @@ if($_SERVER['REQUEST_METHOD'] === 'GET'){
 }
 
 
-$host = $_ENV['PG_HOST'];
-$port = $_ENV['PG_PORT'];
-$db = $_ENV['PG_DB'];
-$user = $_ENV['PG_USER'];
-$password = $_ENV['PG_PASSWORD'];
-$endpoint = $_ENV['PG_ENDPOINT'];
 
-$connection_string = "host=" . $host . " port=" . $port . " dbname=" . $db . " user=" . $user . " password=" . $password . " options='endpoint=" . $endpoint . "' sslmode=require";
-$dbconn = pg_connect($connection_string);
-
-if (!$dbconn) {
-    die("Connection failed: " . pg_last_error());
-}
-
-echo "Connected successfully";
-
-/*
-    "PG_HOST": "ep-shrill-disk-******.eu-central-1.aws.neon.tech",
-    "PG_PORT": "5432",
-    "PG_DB": "php-neon",
-    "PG_USER": "daniil.bazhenov",
-    "PG_PASSWORD": "**********",
-    "PG_ENDPOINT": "ep-shrill-disk-******"*/
 ?>
