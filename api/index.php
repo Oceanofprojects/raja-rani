78c40d0229711e220a9ac6dfea192842c26d80c0
Oceanofprojects <projectocean101@gmail.com>
2024-03-20 17:42:09 +0530

    PDO added

--- api/index.php
+++ api/index.php
@@ -18,21 +18,31 @@ if($_GET['api'] == 'test'){
 	);
 	echo json_encode($response);
 }else if($_GET['api'] && $_GET['api'] == 'live'){
-	$host = $_ENV['PG_HOST'];
-	$port = $_ENV['PG_PORT'];
-	$db = $_ENV['PG_DB'];
-	$user = $_ENV['PG_USER'];
-	$password = $_ENV['PG_PASSWORD'];
-	$endpoint = $_ENV['PG_ENDPOINT'];
+	        $con = 'mysql:host=localhost;dbname=id20369875_ecom;';
+        try {
+            $connection = new PDO($con,'id20369875_ecom','Vimalamani@9050');
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo json_encode(['flag'=>true,'data'=>$connection]); //return Connection
+        } catch (PDOException $e) {
+            //We throw the exception
+            throw new Exception('Problem establishing the connection.');
+        }
+	// $host = $_ENV['PG_HOST'];
+	// $port = $_ENV['PG_PORT'];
+	// $db = $_ENV['PG_DB'];
+	// $user = $_ENV['PG_USER'];
+	// $password = $_ENV['PG_PASSWORD'];
+	// $endpoint = $_ENV['PG_ENDPOINT'];
 
-	$connection_string = "host=" . $host . " port=" . $port . " dbname=" . $db . " user=" . $user . " password=" . $password . " options='endpoint=" . $endpoint . "' sslmode=require";
-	$dbconn = pg_connect($connection_string);
+	// $connection_string = "host=" . $host . " port=" . $port . " dbname=" . $db . " user=" . $user . " password=" . $password . " options='endpoint=" . $endpoint . "' sslmode=require";
+	// $dbconn = pg_connect($connection_string);
 
-	if (!$dbconn) {
-	    die("Connection failed: " . pg_last_error());
-	}
+	// if (!$dbconn) {
+	//     die("Connection failed: " . pg_last_error());
+	// }
 
-	echo "Connected successfully";
+	// echo "Connected successfully";
+
 
 }else{
 	http_response_code(405);
