ebcde42e28309661a2183eeab92417f160572249
Oceanofprojects <115060426+Oceanofprojects@users.noreply.github.com>
2024-03-22 22:37:19 +0530

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,5 +1,5 @@
 <?php
-//print_r($_REQUEST);exit;
+print_r($_REQUEST);exit;
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
